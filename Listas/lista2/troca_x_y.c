0c191417853a384c9438f6fe4fc5e7eade78b395
Gotc2607 <giovanicoelho2005@gmail.com>
2025-10-09 18:23:03 -0300

    feat: exercicio trox por y

---
+++ Listas/lista2/troca_x_y.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+void troca(char *str){
+    if(*str == '\0')
+        return;
+    
+    if(*str == 'x'){
+        *str = 'y';
+    }
+
+    troca(str + 1);
+
+}
+
+int main(){
+
+    char str[80];
+    scanf("%79s", str);
+
+    troca(str);
+    printf("%s\n", str);
+
+
+    return 0;
+}
